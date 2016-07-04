4e601e803e07469329835a96d3170944fb17ac0b
johnnytal <johnnytal9@gmail.com>
2016-07-04 19:44:22 +0200

    ogg!

--- js/preload.js
+++ js/preload.js
@@ -20,26 +20,26 @@ preloader.prototype = {
         game.load.image('slider', 'images/slider.png');
         game.load.image('shuffleBtn', 'images/shuffle.png');
 
-        game.load.audio('sound1', ['audio/metronome1.mp3', 'audio/metronome1.ogg']);
-        game.load.audio('sound2', ['audio/metronome2.mp3', 'audio/metronome2.ogg']);
+        game.load.audio('sound1', ['audio/metronome1.ogg', 'audio/metronome1.mp3']);
+        game.load.audio('sound2', ['audio/metronome2.ogg', 'audio/metronome2.mp3']);
 
-        game.load.audio('sound3', ['audio/can.mp3','audio/can.ogg']);
-        game.load.audio('sound4', ['audio/can2.mp3', 'audio/can2.ogg']);
-        game.load.audio('sound5', ['audio/celery.mp3', 'audio/celery.ogg']);
-        game.load.audio('sound6', ['audio/cork.mp3', 'audio/cork.ogg']);
-        game.load.audio('sound7', ['audio/cork2.mp3', 'audio/cork2.ogg']);
-        game.load.audio('sound8', ['audio/glass.mp3', 'audio/glass.ogg']);
-        game.load.audio('sound9', ['audio/amp.mp3', 'audio/amp.ogg']);
-        game.load.audio('sound10', ['audio/bing.mp3', 'audio/bing.ogg']);
-        game.load.audio('sound11', ['audio/scratch.mp3', 'audio/scratch.ogg']);
-        game.load.audio('sound12', ['audio/tape.mp3', 'audio/tape.ogg']);
-        game.load.audio('sound13', ['audio/puncher.mp3', 'audio/puncher.ogg']);
-        game.load.audio('sound14', ['audio/stapler.mp3', 'audio/stapler.ogg']);
-        game.load.audio('sound15', ['audio/pong.mp3', 'audio/pong.ogg']);
-        game.load.audio('sound16', ['audio/cookoo.mp3', 'audio/cookoo.ogg']);
-        game.load.audio('sound17', ['audio/timpani.mp3', 'audio/timpani.ogg']);
-        game.load.audio('sound18', ['audio/jaw2.mp3', 'audio/jaw2.ogg']);
-        game.load.audio('sound19', ['audio/jaw1.mp3', 'audio/jaw1.ogg']);
+        game.load.audio('sound3', ['audio/can.ogg','audio/can.mp3']);
+        game.load.audio('sound4', ['audio/can2.ogg', 'audio/can2.mp3']);
+        game.load.audio('sound5', ['audio/celery.ogg', 'audio/celery.mp3']);
+        game.load.audio('sound6', ['audio/cork.ogg', 'audio/cork.mp3']);
+        game.load.audio('sound7', ['audio/cork2.ogg', 'audio/cork2.mp3']);
+        game.load.audio('sound8', ['audio/glass.ogg', 'audio/glass.mp3']);
+        game.load.audio('sound9', ['audio/amp.ogg', 'audio/amp.mp3']);
+        game.load.audio('sound10', ['audio/bing.ogg', 'audio/bing.mp3']);
+        game.load.audio('sound11', ['audio/scratch.ogg', 'audio/scratch.mp3']);
+        game.load.audio('sound12', ['audio/tape.ogg', 'audio/tape.mp3']);
+        game.load.audio('sound13', ['audio/puncher.ogg', 'audio/puncher.mp3']);
+        game.load.audio('sound14', ['audio/stapler.ogg', 'audio/stapler.mp3']);
+        game.load.audio('sound15', ['audio/pong.ogg', 'audio/pong.mp3']);
+        game.load.audio('sound16', ['audio/cookoo.ogg', 'audio/cookoo.mp3']);
+        game.load.audio('sound17', ['audio/timpani.ogg', 'audio/timpani.mp3']);
+        game.load.audio('sound18', ['audio/jaw2.ogg', 'audio/jaw2.mp3']);
+        game.load.audio('sound19', ['audio/jaw1.ogg', 'audio/jaw1.mp3']);
     },
     
     create: function(){
